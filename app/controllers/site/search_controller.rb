c32c991a5dd0dcb3a5ec6c811e99ffb0f4ce76fb
Caio <caio.felipemotta@hotmail.com>
2019-07-23 22:59:36 -0300

    194. Fazendo a pesquisa funcionar

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,8 @@
 class Site::SearchController < SiteController
   def questions
     @questions = Question.includes(:answers)
+                         .where('lower(description) LIKE ?', "%#{params[:term].downcase}%")
+                         .page(params[:page])
+                         .per(10)
   end
 end
