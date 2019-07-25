073c5c98811d89dda23b195a7e7af22190e5b557
Caio <caio.felipemotta@hotmail.com>
2019-07-24 22:46:11 -0300

    203. Reflexão sobre o uso do sistema

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//rails-ujs é responsavel pelas requisições remotas à *js.erb
 //= require rails-ujs
 //= require activestorage
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,17 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  scope :search, lambda { |page, term|
+    includes(:answers)
+      .where('lower(description) LIKE ?', "%#{term.downcase}%")
+      .page(page)
+      .per(10)
+  }
+
+  scope :last_questions, lambda { |page|
+    includes(:answers)
+      .order('created_at desc')
+      .page(page).per(5)
+  }
 end
