1f25eea34a1ef718eadb9d73b6d35643259fafcc
Caio <caio.felipemotta@hotmail.com>
2019-07-30 00:02:47 -0300

    Corrige font-awesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -39,4 +39,6 @@ Rails.application.config.assets.precompile += %w[sb-admin-2.js
 # /vendor/assets
 Rails.application.config.assets.precompile += %w[jquery-2.2.4/dist/jquery.js
                                             bootstrap-3.4.1/dist/js/bootstrap.js
-                                            bootstrap-3.4.1/dist/css/bootstrap.css]
+                                            bootstrap-3.4.1/dist/css/bootstrap.css
+                                            @fortawesome/fontawesome-free/css/all.css
+                                            font-awesome/css/font-awesome.css]
